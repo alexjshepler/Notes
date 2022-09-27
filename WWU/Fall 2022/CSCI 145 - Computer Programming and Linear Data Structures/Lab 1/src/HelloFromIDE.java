ec4d52ff75dc652e81116f768acc8764233992db
alexjshepler <alexjshepler@live.com>
2022-09-27 16:38:26 -0700

    Lab 1 complete and middle of class COMM notes

---
+++ WWU/Fall 2022/CSCI 145 - Computer Programming and Linear Data Structures/Lab 1/src/HelloFromIDE.java
@@ -0,0 +1,6 @@
+public class HelloFromIDE {
+    public static void main (String[] args) {
+        System.out.println("Hello world from an IDE!");
+        System.out.println("This is: Alex Shepler");
+    }
+}
